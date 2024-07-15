3f0fbd6eb5c6eda7be286c2cb3fbe253dff113db
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-07-15 21:18:43 +0900

    [AuthorInfo] 작성자 & 공유버튼 UI 디자인

--- src/components/NewsDetail/AuthorInfo.jsx
+++ src/components/NewsDetail/AuthorInfo.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import Share from "./Share";
+import authorImg from "../../assets/author-img.webp";
+import "./AuthorInfo.css";
 
 const AuthorInfo = () => {
   return (
-    <section className="Author-info">
-      <div className="author">Jake Richardson</div>
+    <section className="author-info">
+      <div className="author">
+        <div className="author-img">
+          <img src={authorImg} alt="author-img" />
+        </div>
+        <div className="author-name">Jake Richardson</div>
+      </div>
       <div className="author-shortcut">
         <Share />
-        <a href="/">카테고리명</a>
+        <div className="category-shortcut">
+          <a href="/">카테고리명</a>
+        </div>
       </div>
     </section>
   );
