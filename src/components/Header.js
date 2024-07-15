8813f05cb0bc722514c7421e4cf3046f784bf4ee
aa1as <sm13929881@gmail.com>
2024-07-15 17:36:15 +0900

    [Header.js] wixblog 로고 className, css 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ import classes from "./Header.module.css";
 
 const Header = () => {
   return (
-    <div className={classes.title}>
-      <img src="icon/wixblog.svg" alt="wixblog" />
+    <div className={classes.titleBox}>
+      <img src="icon/wixblog.svg" alt="wixblog" className={classes.title} />
       <div>
         <button className={classes.subscribeButton}>구독하기</button>
         <button className={classes.produceButton}>웹사이트 제작하기</button>
