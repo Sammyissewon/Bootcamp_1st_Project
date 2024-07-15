8b5a733662bb917458d49bfc2e72c7cc11fae6ea
aa1as <sm13929881@gmail.com>
2024-07-15 17:12:34 +0900

    [Header.js] wixblog로고 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,16 @@
 import React from "react";
+import classes from "./Header.module.css";
 
-const header = () => {
-  return <div></div>;
+const Header = () => {
+  return (
+    <div className={classes.title}>
+      <img src="icon/wixblog.svg" alt="wixblog" />
+      <div>
+        <button>구독하기</button>
+        <button>웹사이트 제작하기</button>
+      </div>
+    </div>
+  );
 };
 
-export default header;
+export default Header;
