fe04d1055706659ea4759a1a79a44cb27e740a25
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-07-15 21:19:31 +0900

    [Latest] 최근 게시물 UI 디자인

--- src/components/NewsDetail/Latest.jsx
+++ src/components/NewsDetail/Latest.jsx
@@ -1,21 +1,26 @@
 import React from "react";
+import "./Latest.css";
 
 const Latest = () => {
   return (
-    <section className="latest">
-      <div>
+    <section className="latest-wrapper">
+      <div className="latest">
         <div>최근 게시물</div>
         <a href="/">전체 보기</a>
       </div>
       <div className="latest-thumb">
-        <img
-          src="https://paultan.org/image/2024/02/2024-BYD-Yuan-Up-China-reveal-1-1200x628.jpg"
-          alt="latest-thumb"
-        />
+        <a href="/">
+          <img
+            src="https://paultan.org/image/2024/02/2024-BYD-Yuan-Up-China-reveal-1-1200x628.jpg"
+            alt="latest-thumb"
+          />
+        </a>
       </div>
       <div className="latest-title">
-        Just 19 out of 137 Chinese EV brands to stay profitable by 2030 due to
-        brutal price war – AlixPartners report
+        <a href="/">
+          Just 19 out of 137 Chinese EV brands to stay profitable by 2030 due to
+          brutal price war – AlixPartners report
+        </a>
       </div>
     </section>
   );
