f21ae05152b80078b3e4260dfdf5db986dca7fde
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-07-15 21:19:50 +0900

    [Review] 댓글 영역 UI 디자인

--- src/components/NewsDetail/Review.jsx
+++ src/components/NewsDetail/Review.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import "./Review.css";
 
 const Review = () => {
   return (
     <section className="review">
       <div className="review-title">댓글</div>
-      <div className="review-input">
-        <input type="text" />
+      <div className="review-input-wrapper">
+        <div>댓글을 입력하세요</div>
       </div>
     </section>
   );
