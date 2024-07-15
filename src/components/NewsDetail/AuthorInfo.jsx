1f5b6d78f0731edf059ce14b994ef6e5b8649d2c
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-07-15 15:19:26 +0900

    [AuthorInfo] 하드코딩으로 내용추가

--- src/components/NewsDetail/AuthorInfo.jsx
+++ src/components/NewsDetail/AuthorInfo.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Share from "./Share";
 
 const AuthorInfo = () => {
-  return <div>editorinfo</div>;
+  return (
+    <section className="Author-info">
+      <div className="author">Jake Richardson</div>
+      <div className="author-shortcut">
+        <Share />
+        <a href="/">카테고리명</a>
+      </div>
+    </section>
+  );
 };
 
 export default AuthorInfo;
