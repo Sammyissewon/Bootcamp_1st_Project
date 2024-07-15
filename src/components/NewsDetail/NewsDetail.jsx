9f4c9686c45179eb226f88c72859d227183e713e
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-07-15 15:21:34 +0900

    [NewsInfo] 공유버튼이미지, 사이드이미지 추가

--- src/components/NewsDetail/NewsDetail.jsx
+++ src/components/NewsDetail/NewsDetail.jsx
@@ -3,14 +3,20 @@ import NewsInfo from "./NewsInfo";
 import AuthorInfo from "./AuthorInfo";
 import Latest from "./Latest";
 import Review from "./Review";
+import asideImg from "../../assets/aside-img.webp";
 
 const NewsDetail = () => {
   return (
     <>
-      <NewsInfo />
-      <AuthorInfo />
-      <Latest />
-      <Review />
+      <div className="news-detail">
+        <NewsInfo />
+        <AuthorInfo />
+        <Latest />
+        <Review />
+      </div>
+      <div className="news-detail-aside">
+        <img src={asideImg} alt="aside-img" />
+      </div>
     </>
   );
 };
