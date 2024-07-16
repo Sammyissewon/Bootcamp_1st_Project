02348fca1c8cdc9d4106812f2000279edc469fa6
aa1as <sm13929881@gmail.com>
2024-07-16 16:41:15 +0900

    [modal] 초록색 modal ui 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Modal from "./components/Modal";
 
 function App() {
   return (
     <div>
       <Header />
+      <Modal />
       <Footer />
     </div>
   );
