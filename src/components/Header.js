92f9e3afb72971db23b095c908268a608aae9356
hbin3673 <hbin3673@gmail.com>
2024-07-16 16:35:02 +0900

    [Header] 디자인 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
 
 const Header = () => {
   return (
     <header>
       <div className="header">가입하기</div>
-      <div className='h2'>이미 사용 중인 계정이 있다면 로그인하세요.</div>
+      <div className="message">
+      이미 사용 중인 계정이 있다면 <span className='login'>로그인</span>하세요.
+      </div>
     </header>
   );
 };
