d25a5af429c85ac21a00ac290df5abfd71fc7fd1
Sammyissewon <person1341@gmail.com>
2024-07-16 10:51:34 +0700

    [MainItemList] News API fetch 해오기

--- src/Page/MainItemList.js
+++ src/Page/MainItemList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 // components
 import Item from "../Components/Item"; // Item 컴포넌트를 불러옴
@@ -6,13 +7,33 @@ import Item from "../Components/Item"; // Item 컴포넌트를 불러옴
 const items = Array.from({ length: 20 }); // 16개의 항목 생성
 
 const MainItemList = () => {
+  // 뉴스 API 불러오기
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${
+      import.meta.env.REACT_APP_API_KEY
+    }`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setArticles(data.articles));
+  }, []);
+
   return (
     <>
       <div className="MainItemList">
         <div className="grid-container">
-          {items.map((_, index) => (
-            <Item key={index} />
-          ))}
+          {articles.map((news, index) => {
+            return (
+              // fetch 해온 source의 Key값을 활용하여, Props로 전달
+              <Item
+                key={index}
+                title={news.title}
+                src={news.urlToImage}
+                date={news.publishedAt}
+                url={news.url}
+              />
+            );
+          })}
         </div>
       </div>
     </>
