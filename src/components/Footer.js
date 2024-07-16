bfdef26f6c458be6cc2c4ec0a62ec5ad08eeb0cd
hbin3673 <hbin3673@gmail.com>
2024-07-16 16:32:26 +0900

    [Footer] 내용 작성 및 hover시 색상 변경되도록 수정

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import './Footer.css';
+import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>© 2024 Wix. All rights reserved.</p>
+      <p className="plan">
+        * 가입 시 Wix <span className="line">이용약관</span>에 동의하며{" "}
+        <span className="line">개인정보처리방침</span>을 확인한 것으로
+        간주됩니다.
+        <br />본 사이트는 reCAPTCHA Enterprise로 보호되며,{" "}
+        <span className="line">Google 개인정보처리방침</span> 및 서비스 약관의
+        적용을 받습니다.
+      </p>
     </footer>
   );
 };
