a6e4c4184b2cb59732874347847f45de1019baa8
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-07-16 23:13:23 +0900

    [NewsInfo] Redux store로 값 가져오는 기능 추가

--- src/components/NewsDetail/NewsInfo.jsx
+++ src/components/NewsDetail/NewsInfo.jsx
@@ -2,33 +2,33 @@ import React from "react";
 import Share from "./Share";
 import shareImg from "../../assets/share.svg";
 import "./NewsInfo.css";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const NewsInfo = () => {
+  const { id } = useParams();
+  const newsState = useSelector((state) => state.news.newsState);
+  const article = newsState[id];
+
+  if (!article) {
+    return <div>기사가 존재하지 않습니다.</div>;
+  }
+
   return (
     <section className="news-info-wrapper">
       <div className="news-info">
-        <div className="news-date">2024-07-15T00:16:32Z</div>
+        <div className="news-date">{article.publishedAt}</div>
         <div className="share-btn">
           <img src={shareImg} alt="share-btn" />
           <Share />
         </div>
       </div>
-      <div className="news-title">
-        Another Tesla With Over 400,000 Miles On One Battery
-      </div>
+      <div className="news-title">{article.title}</div>
       <div className="news-thumb">
-        <img
-          src="https://cleantechnica.com/wp-content/uploads/2024/04/Tesla-Model-3-White-Tesla-Model-S-Psychadelic-Supercharging-2-CleanTechnica-watermark-new.jpeg"
-          alt="news-thumb"
-        />
+        <img src={article.urlToImage} alt={article.title} />
       </div>
       <div className="news-con">
-        <p>
-          Sign up for daily news updates from CleanTechnica on email. Or follow
-          us on Google News!\r\nA week ago, I wrote an article about a guy in
-          the UK with two high-mileage Tesla Model S sedans that are stil…
-          [+5552 chars]
-        </p>
+        <p>{article.description}</p>
       </div>
     </section>
   );
