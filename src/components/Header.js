ac24b48b8a40f6a040b714753c3061c54c69408d
hbin3673 <hbin3673@gmail.com>
2024-07-16 14:45:53 +0900

    [Header] 회원가입 페이지의 헤더부분 초안

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header>
+      <div className="header">가입하기</div>
+      <div className='h2'>이미 사용 중인 계정이 있다면 로그인하세요.</div>
+    </header>
+  );
+};
+
+export default Header;
