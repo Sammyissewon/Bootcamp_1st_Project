6a9c44fcdb97226c51497864e9a95ef69fca2248
hbin3673 <hbin3673@gmail.com>
2024-07-16 14:08:27 +0900

    [Subscribe] UI 수정 및 아이콘 추가

--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -1,41 +1,62 @@
-import React, { useState } from 'react';
-import './Subscribe.css'
-
-
+import React, { useState } from "react";
+import "./Subscribe.css";
 
 const Subscribe = () => {
-    const [email, setEmail] = useState('');
+  const [email, setEmail] = useState("");
 
-    const handleInputChange = (e) => {
-      setEmail(e.target.value);
-    };
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      // 이메일 제출 처리 (예: API 호출)
-      console.log('구독할 이메일:', email);
-      // 이메일 입력 후 초기화
-      setEmail('');
-    };
+  const handleInputChange = (e) => {
+    setEmail(e.target.value);
+  };
 
-    return (
-        <div className='Subscribe'>
-            <div className='head'>Wix 블로그<br />이메일로 구독하세요!</div>
-            <div className='sub'>
-                Wix 홈페이지 제작 및 마케팅에 관한 새로운 콘텐<br />츠를 메일로 편하게 받아보실 수 있습니다.</div>
-            <form onSubmit={handleSubmit} className='form-container'>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // 이메일 제출 처리 (예: API 호출)
+    console.log("구독할 이메일:", email);
+    // 이메일 입력 후 초기화
+    setEmail("");
+  };
+
+  return (
+    <div className="Subscribe">
+      <div className="head">
+        Wix 블로그
+        <br />
+        이메일로 구독하세요!
+      </div>
+      <div className="sub">
+        Wix 홈페이지 제작 및 마케팅에 관한 새로운 콘텐
+        <br />
+        츠를 메일로 편하게 받아보실 수 있습니다.
+      </div>
+      <form onSubmit={handleSubmit} className="form-container">
         <input
-          type='email'
-          placeholder=' 이메일 주소를 입력해 주세요'
-          className='email-input'
+          type="email"
+          placeholder=" 이메일 주소를 입력해 주세요"
+          className="email-input"
           value={email}
           onChange={handleInputChange}
           required
         />
-        <button type='submit' className='subscribe-btn'>구독 신청</button>
+        <button type="submit" className="subscribe-btn">
+          구독 신청
+        </button>
       </form>
-        </div>
-    );
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="size-6"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M6 18 18 6M6 6l12 12"
+        />
+      </svg>
+    </div>
+  );
 };
 
 export default Subscribe;
