35a59f7298fcba6d2bb3a48820bb3ca6d2b84e73
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-07-17 11:51:24 +0900

    [Review.js] jsx -> js 파일로 변경

---
+++ src/components/NewsDetail/Review.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Review.css";
+
+const Review = () => {
+  return (
+    <section className="review">
+      <div className="review-title">댓글</div>
+      <div className="review-input-wrapper">
+        <div>댓글을 입력하세요</div>
+      </div>
+    </section>
+  );
+};
+
+export default Review;
