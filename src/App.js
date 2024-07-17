6a291b117f77339957c062118e0bba4cb4d0aabe
hbin3673 <hbin3673@gmail.com>
2024-07-17 15:10:44 +0900

    [App] 수정수정

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Modal from "./components/Modal";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Modal />
+    </div>
+  );
+}
+
+export default App;
+
+// import React from "react";
+// import "./App.css";
+// import Main from "./Page/Main";
+// import Section from "./Page/Section";
+// import { useState } from "react";
+
+// const App = () => {
+//   return (
+//     <>
+//       <Main />
+//       <Section />
+//     </>
+//   );
+// };
+
+// export default App;
+
+// import "./App.css";
+// import Footer2 from "./components/Footer2";
+// import Header2 from "./components/Header2";
+// import SignupForm from "./components/SignupForm";
+
+// function App() {
+//   return (
+//     <div>
+//       <Header2 />
+//       <Footer2 />
+//       <SignupForm />
+//     </div>
+//   );
+// }
+
+// export default App;
