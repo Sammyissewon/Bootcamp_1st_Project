06915e9bbbf9868fb139b4ed3adc1c6c46e5ef31
Sammyissewon <person1341@gmail.com>
2024-07-17 12:46:48 +0700

    [App.js] 성민님 App.js 병합

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React from "react";
 import "./App.css";
 import Main from "./Page/Main";
 import Section from "./Page/Section";
-import { useState } from "react";
+
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Modal from "./components/Modal";
 
 const App = () => {
   return (
     <>
+      <Header />
+      <Footer />
+      <Modal />
+
       <Main />
       <Section />
     </>
