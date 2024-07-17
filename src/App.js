a3e52bff6dfb52dac6679de7c55415132da1a956
Sammyissewon <person1341@gmail.com>
2024-07-17 12:49:35 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import React from "react";
 import "./App.css";
-import Main from "./Page/Main";
-import Section from "./Page/Section";
-
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Modal from "./components/Modal";
 
-const App = () => {
+function App() {
   return (
-    <>
+    <div>
       <Header />
       <Footer />
       <Modal />
-
-      <Main />
-      <Section />
-    </>
+    </div>
   );
-};
+}
 
 export default App;
+
+// import React from "react";
+// import "./App.css";
+// import Main from "./Page/Main";
+// import Section from "./Page/Section";
+// import { useState } from "react";
+
+// const App = () => {
+//   return (
+//     <>
+//       <Main />
+//       <Section />
+//     </>
+//   );
+// };
+
+// export default App;
