0173569ded5f6a0f2f5254ad26689901ab823b4c
hbin3673 <hbin3673@gmail.com>
2024-07-17 15:00:29 +0900

    [App] 주석 삭제

--- src/App.js
+++ src/App.js
@@ -14,20 +14,3 @@ function App() {
 }
 
 export default App;
-
-// import React from "react";
-// import "./App.css";
-// import Main from "./Page/Main";
-// import Section from "./Page/Section";
-// import { useState } from "react";
-
-// const App = () => {
-//   return (
-//     <>
-//       <Main />
-//       <Section />
-//     </>
-//   );
-// };
-
-// export default App;
