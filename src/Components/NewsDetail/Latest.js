9d4f16e15718d700b76f3524eec7ad316f038ba9
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-07-18 21:46:11 +0900

    [NewsDetail] 빈썸네일 이미지 추가

--- src/Components/NewsDetail/Latest.js
+++ src/Components/NewsDetail/Latest.js
@@ -26,7 +26,14 @@ const Latest = () => {
         <>
           <Link to="/news/0">
             <div className={classes.latestThumb}>
-              <img src={latestArticle.urlToImage} alt={latestArticle.title} />
+              <img
+                src={
+                  latestArticle.urlToImage
+                    ? latestArticle.urlToImage
+                    : "../assets/news-image.webp"
+                }
+                alt={latestArticle.title}
+              />
             </div>
           </Link>
           <div className={classes.latestTitle}>
