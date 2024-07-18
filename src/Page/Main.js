17d8de1e02a20c971141f3af68299d828538865f
Sammyissewon <person1341@gmail.com>
2024-07-18 19:28:20 +0700

    [Main] 로딩메시지 + 첫 렌더링 오류 수정

--- src/Page/Main.js
+++ src/Page/Main.js
@@ -1,8 +1,8 @@
 import React from "react";
-import classes from "./Main.module.css";
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import classes from "./Main.module.css";
 
 // components
 import MainItemList from "./MainItemList";
@@ -14,7 +14,7 @@ const Main = () => {
   // Redux state 불러오기
   const newsState = useSelector((state) => state.news.newsState);
   // 1번째 최신 기사
-  const mainArticle = newsState[0];
+  const mainArticle = newsState && newsState.length > 0 ? newsState[0] : null;
 
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
@@ -23,19 +23,25 @@ const Main = () => {
   return (
     <div>
       <div className={classes.upperSide}>
-        <div className={classes.leftSide}>
-          <Link to="/news/0">
-            <h4 className={classes.mainArticle}>{mainArticle.title}</h4>
-          </Link>
-          <Link to="/news/0">
-            <button className={classes.detailButton}>자세히 보기 </button>
-          </Link>
-        </div>
-        <img
-          src={mainArticle.urlToImage}
-          className={classes.mainImg}
-          alt="main img"
-        ></img>
+        {mainArticle ? (
+          <>
+            <div className={classes.leftSide}>
+              <Link to="/news/0">
+                <h4 className={classes.mainArticle}>{mainArticle.title}</h4>
+              </Link>
+              <Link to="/news/0">
+                <button className={classes.detailButton}>자세히 보기</button>
+              </Link>
+            </div>
+            <img
+              src={mainArticle.urlToImage}
+              className={classes.mainImg}
+              alt="main img"
+            />
+          </>
+        ) : (
+          <div>로딩 중...</div>
+        )}
       </div>
 
       <MainItemList className={classes.mainItemList} category="general" />
