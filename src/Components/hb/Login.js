f442e0b04308b38d0dc2eb03f01c81a2c2ec9a60
hbin3673 <hbin3673@gmail.com>
2024-07-18 20:37:48 +0900

    [Login] 로그인 화면 디자인

--- src/Components/hb/Login.js
+++ src/Components/hb/Login.js
@@ -1,8 +1,34 @@
-import React from "react";
-import "./Login.css";
+import React, { useState } from "react";
+import classes from "./Login.module.css";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div></div>;
+
+  const [email, setEmail] = useState("");
+
+  return <div className={classes.main}>
+    <img src="/icon/wix.svg" alt="wix" />
+          <div className={classes.header}>로그인</div>
+          <div className={classes.message}>
+            아직 계정이 없으신가요? 
+            <Link to={"/SignupForm"}>
+            <span className={classes.signupForm}> 가입하기</span></Link>
+          </div>
+
+          <div className={classes.formGroup}>
+            <input
+              type="email"
+              placeholder="이메일"
+              className={classes.email}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <span className={classes.password}>비밀번호 찾기</span>
+
+
+  </div>;
 };
 
 export default Login;
